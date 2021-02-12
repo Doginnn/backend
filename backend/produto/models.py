518836bb1ac231e715702ac5ed08bce3a5b6faa3
doginnn <diogenesemmanuel@gmail.com>
2021-02-12 10:16:56 -0300

    [ADD]Criando modelo e registrando no admin.

--- backend/produto/models.py
+++ backend/produto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome do Produto:')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Valor:', help_text='Ex.: 123.45')
+    image_url = models.ImageField(verbose_name='Imagem:')
+
+    def __str__(self):
+        return self.name
