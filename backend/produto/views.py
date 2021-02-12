8d55c1cab334292657c1ffdf735386e269c22ba7
doginnn <diogenesemmanuel@gmail.com>
2021-02-12 10:45:31 -0300

    [UPD]Atualizando a views.py

--- backend/produto/views.py
+++ backend/produto/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Produto
+from .serializers import ProdutoSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class ProdutoList(generics.ListAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+
+class ProdutoListCreate(generics.ListCreateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+
+class ProdutoRetrieveUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+
+class ProdutoDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+    def delete(self, request, pk=None, format=None):
+        product = get_object_or_404(Produto, id=pk)
+        product.delete()
+        return Response({'message':'product deleted'})
